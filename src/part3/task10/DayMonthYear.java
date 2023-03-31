ad5d4f1558b1a5a7445f133e3fdd14b83a95bcea
Polina Buber <buberp0@gmail.com>
2023-04-01 01:30:43 +0300

    ///

--- src/part3/task10/DayMonthYear.java
+++ src/part3/task10/DayMonthYear.java
@@ -4,22 +4,32 @@ import java.util.Calendar;
 
 public class DayMonthYear {
     public static void main(String[] args) {
-        int day = 5;
-        int month = 7;
-        int year = 2019;
-        //TODO: JULY 32 !!!!
+        int day = 30;
+        int month = 6;
+        int year = 2020;
 
         if (day == 28 && month == 2 && (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))) {
             day++;
             System.out.println("Високосный год. Следующий день: " + day + "." + month + "." + year);
-        } else if ((day >= 1 && day <= 31) && (month >= 1 && month <= 12)) {
-            day++;
-            System.out.println("Следующий день: " + day + "." + month + "." + year);
-        }  else if ((day == 30) && (month == 2 || month == 5 || month == 9 || month == 11)) {
+        } else if (day == 29 && month == 2 && (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))) {
+            day = 1;
+            month++;
+            System.out.println("Високосный год. Следующий день: " + day + "." + month + "." + year);
+        } else if ((day == 28) && (month == 2)) {
             day = 1;
             month++;
             System.out.println("Следующий день: " + day + "." + month + "." + year);
-        } else if ((day == 31) && (month == 1 || month == 3 || month == 4 || month == 8 || month == 10)) {
+        } else if ((day > 0 && day < 28) && (month == 2)) {
+            day++;
+            System.out.println("Следующий день: " + day + "." + month + "." + year);
+        } else if ((day == 30) && (month == 5 || month == 6 || month == 9 || month == 11)) {
+            day = 1;
+            month++;
+            System.out.println("Следующий день: " + day + "." + month + "." + year);
+        }else if ((day > 0 && day < 30) && (month == 5 || month == 6 || month == 9 || month == 11)) {
+            day++;
+            System.out.println("Следующий день: " + day + "." + month + "." + year);
+        } else if ((day == 31) && (month == 1 || month == 3 || month == 4 || month == 7 || month == 8 || month == 10)) {
             day = 1;
             month++;
             System.out.println("Следующий день: " + day + "." + month + "." + year);
@@ -28,13 +38,11 @@ public class DayMonthYear {
             month = 1;
             year++;
             System.out.println("Следующий день: " + day + "." + month + "." + year);
-
-        } else {
-            day = 1;
-            month++;
+        }  else if ((day > 0 && day < 31) && (month == 1 || month == 3 || month == 4 || month == 7 || month == 8 || month == 10 || month == 12)) {
+            day++;
             System.out.println("Следующий день: " + day + "." + month + "." + year);
+        } else {
+            System.out.println("Введите корректную дату");
         }
-
-
     }
 }
