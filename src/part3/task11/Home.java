f6fbca85a4b4b15f14e097d7a73789abf78fab5a
Polina Buber <buberp0@gmail.com>
2023-04-02 13:30:28 +0300

    ///

--- src/part3/task11/Home.java
+++ src/part3/task11/Home.java
@@ -6,24 +6,16 @@ public class Home {
         int c = 10, d = 15;
         int e = 20, f = 25;
 
-        if (f >= (b + d) && (e >= a && e >= c)) {
+        if (f >= (a + c) && (e >= b || e >= d)) {
             System.out.println("Дома помещаются");
-        } else if (f >= (a + c) && (e >= b && e >= d)) {
-            System.out.println("Дома помещаются");
-        } else if (f >= (b + c) && (e >= a && e >= d)) {
-            System.out.println("Дома помещаются");
-        } else if (f >= (a + d) && (e >= c && e >= b)) {
-            System.out.println("Дома помещаются");
-        } else if (e >= (b + d) && (f >= a && f >= c)) {
-            System.out.println("Дома помещаются");
-        } else if (e >= (a + d) && (f >= b && f >= c)) {
-            System.out.println("Дома помещаются");
-        } else if (e >= (b + c) && (f >= d && f >= a)) {
-            System.out.println("Дома помещаются");
-        } else if (e >= (a + c) && (f >= b && f >= d)) {
+        } else if (e >= (b + d) && (f >= a || f >= c)) {
             System.out.println("Дома помещаются");
         } else {
             System.out.println("Дома не помещаются");
         }
+        //Если можно ставить дом на дом, то можно сделать одну
+        //проверку
+        //if ((e >= a && e >= c) && (f >= b && f >= d)
+        //System.out.println("Дома помещаются");
     }
 }
