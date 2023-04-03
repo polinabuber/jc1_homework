2110f987814bd3431e08e25e2ae7bd50dad1f250
Polina Buber <buberp0@gmail.com>
2023-04-03 03:09:21 +0300

    ///

--- src/part4/task18/RandomNumber.java
+++ src/part4/task18/RandomNumber.java
@@ -1,9 +1,24 @@
 package part4.task18;
 
-import java.text.DecimalFormat;
 public class RandomNumber {
     public static void main(String[] args) {
-        int num = 12345678;
-        System.out.println(new DecimalFormat("#,###").format(num));
+        int num = 123455555;
+        int i = 0;
+        int j;
+        String accounting = "";
+
+        while (num > 0) {
+            j = num % 10;
+            accounting = j + accounting;
+            num = num / 10;
+            i++;
+            if (i % 3 == 0 && num != 0) {
+                accounting = " " + accounting;
+            }
+        }
+        System.out.println(accounting);
     }
+       /* int num = 12345678;
+        System.out.println(new DecimalFormat("#,###").format(num));*/
 }
+
