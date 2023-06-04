4eb5f05fb431b9a6087038c4fac491374077f84c
Polina Buber <buberp0@gmail.com>
2023-06-04 19:23:31 +0300

    ///

--- src/part16/task64/Main.java
+++ src/part16/task64/Main.java
@@ -3,6 +3,7 @@ package part16.task64;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
         int n = 100;
