137ca6fdf360e29864fcfb95e52af727091733f8
Polina Buber <buberp0@gmail.com>
2023-06-04 19:22:02 +0300

    ///

--- src/part16/task63/Main.java
+++ src/part16/task63/Main.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
         Random random = new Random();
