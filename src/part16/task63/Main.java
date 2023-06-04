cda029c2f7b013e1c69f180c4b3a55a6ea9c0c7b
Polina Buber <buberp0@gmail.com>
2023-06-04 19:20:27 +0300

    ///

---
+++ src/part16/task63/Main.java
@@ -0,0 +1,29 @@
+package part16.task63;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+        List<Person> people = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            String name = "Имя " + random.nextInt(100);
+            String surname = "Фамилия " + random.nextInt(100);
+            int age = 15 + random.nextInt(16);
+            people.add(new Person(name, surname, age));
+        }
+
+        List<String> surnames = people.stream()
+                .filter(p -> p.age < 21)
+                .peek(System.out::println)
+                .sorted(Comparator.comparing((Person p) -> p.surname).thenComparing(p -> p.name))
+                .limit(4)
+                .map(p -> p.surname)
+                .collect(Collectors.toList());
+
+        System.out.println(surnames);
+    }
+}
---
+++ src/part16/task64/Main.java
@@ -0,0 +1,21 @@
+package part16.task64;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+public class Main {
+    public static void main(String[] args) {
+        int n = 100;
+        int minValue = 1;
+        int maxValue = 1000;
+
+        List<Integer> numbers = IntStream.rangeClosed(minValue, maxValue)
+                .boxed()
+                .limit(n)
+                .collect(Collectors.toList());
+
+        boolean trueOrFalse = numbers.stream()
+                .anyMatch(i -> i % 3 == 0 && i % 5 == 0);
+        System.out.println(trueOrFalse);
+    }
+}
