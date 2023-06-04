b7519db382fb14d12334acb7b0170a45af0df2c3
Polina Buber <buberp0@gmail.com>
2023-06-04 18:35:27 +0300

    ///

--- src/progress/Progress.java
+++ src/progress/Progress.java
@@ -2,9 +2,9 @@ package progress;
 
 public class Progress {
     public static void main(String[] args) {
-        int[] nums = {10, 10, 6, 10, 10, 8, 10, 10, 5, 10, 4, 10, 10, 10, 10, 10, 10, 5, 10, 10, 6, 10, 10, 9, 7, 10, 10, 10, 10, 10, 10, 9, 10, 9, 9, 10, 7, 7, 8};
-        int result = 0;
-        for (int m : nums) {
+        double[] nums = {10, 10, 6, 10, 10, 8, 10, 10, 5, 10, 4, 10, 10, 10, 10, 10, 10, 5, 10, 10, 6, 10, 10, 9, 7, 10, 10, 10, 10, 10, 10, 9, 10, 9, 9, 10, 7, 7, 8,10,10,10,9};
+        double result = 0;
+        for (double m : nums) {
             result += m;
         }
         System.out.println("Среднее арифметическое: " + result / nums.length);
