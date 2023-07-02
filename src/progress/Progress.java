943e131aeffab1633bd37824540283e0295156c8
Polina Buber <buberp0@gmail.com>
2023-07-02 22:37:25 +0300

    ///

--- src/progress/Progress.java
+++ src/progress/Progress.java
@@ -2,11 +2,12 @@ package progress;
 
 public class Progress {
     public static void main(String[] args) {
-        double[] nums = {10, 10, 6, 10, 10, 8, 10, 10, 5, 10, 4, 10, 10, 10, 10, 10, 10, 5, 10, 10, 6, 10, 10, 9, 7, 10, 10, 10, 10, 10, 10, 9, 10, 9, 9, 10, 7, 7, 8,10,10,10,9};
+        double[] nums =
+                {10, 10, 6, 10, 10, 8, 10, 10, 5, 10, 4, 10, 10, 10, 10, 10, 10, 5, 10, 10, 6, 10, 10, 9, 7, 10, 10, 10, 10, 10, 10, 9, 10, 9, 9, 10, 7, 7, 8, 10, 10, 10, 9, 10, 10, 10, 9, 9, 10, 9, 9, 9, 10, 10, 9, 6, 10, 8, 10, 10, 10, 10, 10, 10};
         double result = 0;
         for (double m : nums) {
             result += m;
         }
-        System.out.println("Среднее арифметическое: " + result / nums.length);
+        System.out.println("Оценка за курс: " + result / nums.length);
     }
 }
